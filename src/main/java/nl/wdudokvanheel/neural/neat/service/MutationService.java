ed256ee41dee5ebfae2aa36f4897da18ae0c5cb0
wdudokvanheel <wesley@bitechular.com>
2025-07-04 15:49:13 +0200

    Allow for multiple mutations per genome

--- src/main/java/nl/wdudokvanheel/neural/neat/service/MutationService.java
+++ src/main/java/nl/wdudokvanheel/neural/neat/service/MutationService.java
@@ -4,37 +4,50 @@ import nl.wdudokvanheel.neural.neat.model.Genome;
 import nl.wdudokvanheel.neural.neat.model.NeatConfiguration;
 import nl.wdudokvanheel.neural.neat.mutation.*;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class MutationService {
     private Random random = new Random();
-    private final InnovationService innovationService;
-    //    public List<Mutation> mutations = new ArrayList<>();
-    private HashMap<Mutation, Double> mutations = new HashMap<>();
+    private NeatConfiguration configuration;
+    private Map<Mutation, Double> mutations = new LinkedHashMap<>();
 
     public MutationService(NeatConfiguration configuration, InnovationService innovationService) {
-        this.innovationService = innovationService;
+        this.configuration = configuration;
+
         addMutation(new AddNeuronMutation(innovationService), configuration.mutateAddNeuronProbability);
         addMutation(new AddConnectionMutation(innovationService), configuration.mutateAddConnectionProbability);
         addMutation(new ToggleConnectionMutation(), configuration.mutateToggleConnectionProbability);
-        addMutation(new WeightMutation(configuration.mutateRandomizeWeightsProbability, configuration.mutateWeightPerturbationPower), configuration.mutateWeightProbability);
+        addMutation(
+                new WeightMutation(
+                        configuration.mutateRandomizeWeightsProbability,
+                        configuration.mutateWeightPerturbationPower
+                ),
+                configuration.mutateWeightProbability
+        );
     }
 
     private void addMutation(Mutation mutation, double probability) {
         mutations.put(mutation, probability);
     }
 
-    /**
-     * Apply a random mutation to the specified genome
-     */
     public void mutateGenome(Genome genome) {
-        for (Mutation mutation : mutations.keySet()) {
-            double probability = mutations.get(mutation);
-
-            if (random.nextDouble() < probability) {
-                mutation.mutate(genome);
-                return;
+        if (configuration.multipleMutationsPerGenome) {
+            for (var entry : mutations.entrySet()) {
+                if (random.nextDouble() < entry.getValue()) {
+                    entry.getKey().mutate(genome);
+                }
+            }
+        } else {
+            double roll = random.nextDouble();
+            double cumulative = 0;
+            for (var entry : mutations.entrySet()) {
+                cumulative += entry.getValue();
+                if (roll < cumulative) {
+                    entry.getKey().mutate(genome);
+                    break;
+                }
             }
         }
     }
